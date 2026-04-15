a6987d050eaacc6f974a666c19d42c39f0c28de9
Арина <moskalcenkoarina@gmail.com>
2026-04-15 21:14:58 +0400

    docs: update README with build, deploy and development instructions

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar app flat class="soccerstat-navbar">
-      <template #prepend>
+  <div class="app-container">
+    <nav class="soccerstat-navbar">
+      <div class="navbar-container">
         <router-link to="/" class="navbar-logo">
-          <img src="/FIFA.svg" alt="FIFA Logo" class="logo-image" />
+          <img :src="logo" alt="FIFA Logo" class="logo-image" />
         </router-link>
-      </template>
 
-      <div class="navbar-tabs-container">
-        <div class="navbar-tabs">
-          <button 
-            :class="['navbar-tab', { active: isActive('Leagues') }]"
-            @click="navigateTo('Leagues')"
-          >
-            Лиги
-          </button>
-          <button 
-            :class="['navbar-tab', { active: isActive('Teams') }]"
-            @click="navigateTo('Teams')"
-          >
-            Команды
-          </button>
+        <div class="navbar-tabs-container">
+          <div class="navbar-tabs">
+            <button 
+              :class="['navbar-tab', { active: isActive('Leagues') }]"
+              @click="navigateTo('Leagues')"
+            >
+              Лиги
+            </button>
+            <button 
+              :class="['navbar-tab', { active: isActive('Teams') }]"
+              @click="navigateTo('Teams')"
+            >
+              Команды
+            </button>
+          </div>
+          <div class="navbar-indicator" :style="indicatorStyle"></div>
         </div>
-        <div class="navbar-indicator" :style="indicatorStyle"></div>
       </div>
+    </nav>
 
-      <v-spacer />
-    </v-app-bar>
-
-    <v-main class="main-bg">
+    <main class="main-bg">
       <router-view />
-    </v-main>
-  </v-app>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -39,6 +37,7 @@ export default {
   name: 'App',
   data() {
     return {
+      logo: new URL('/FIFA.svg', import.meta.url).href,
       activeTab: 'Leagues',
       indicatorPosition: 0,
       indicatorWidth: 0,
@@ -52,18 +51,18 @@ export default {
       };
     },
   },
-  watch: {
-    '$route.name'() {
-      this.updateActiveTab();
-      this.updateIndicator();
-    },
-  },
   mounted() {
     this.updateIndicator();
     window.addEventListener('resize', this.updateIndicator);
   },
   beforeUnmount() {
     window.removeEventListener('resize', this.updateIndicator);
+  },
+  watch: {
+    '$route.name'() {
+      this.updateActiveTab();
+      this.updateIndicator();
+    },
   },
   methods: {
     isActive(routeName) {
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './css/app.css';
 import './css/league.css';
 import './css/responsive.css';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
 import router from './router';
 
-createApp(App).use(vuetify).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
