f3e68c321b8134fb0ac168a451cbc0550581a413
Арина <moskalcenkoarina@gmail.com>
2026-04-15 21:39:59 +0400

    Finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-// src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
+
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Leagues from '../views/Leagues.vue';
 import Teams from '../views/Teams.vue';
 import LeagueMatches from '../views/LeagueMatches.vue';
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
